f80370f72d77c2f04e8f606c24f512f0556b958a
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-09-10 00:28:57 -0300

    up

--- JavaScript/site-animais-fantasticos/js/script.js
+++ JavaScript/site-animais-fantasticos/js/script.js
@@ -1,6 +1,26 @@
 
-const res = document.querySelector('.menu')
+const tabMenu = document.querySelectorAll('.js-tabmenu li')
+const tabContent = document.querySelectorAll('.js-tabcontent section')
+tabContent[0].classList.add('ativo')
 
-console.log(res.querySelector('li:last-child'))
+//verifica se existem as classes
+if(tabMenu.length && tabContent.length){
+    
+    //evento de click na lista de animais
+    tabMenu.forEach((item, index)=>{
+        item.addEventListener('click', ()=>{
+            activeSection(index)
+        })
+    })
+    //função para add ou remover as sections de animals description
+    function activeSection(index){
+        tabContent.forEach(element => {
+            element.classList.remove('ativo')
+        });
+        tabContent[index].classList.add('ativo')
+    }
+}
 
 
+
+ 
