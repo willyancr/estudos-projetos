ee294708f67bc28d41121797b8a1d91c1236b417
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-09-11 22:41:05 -0300

    up

--- JavaScript/1/script.js
+++ JavaScript/1/script.js
@@ -1,7 +1,59 @@
-const res = document.querySelector('.resultado')
 
-function load(){
-    res.textContent = 'Página carregada'
+function Pessoa(nome, idade){
+
+    this.nome = nome,
+    this.idade = idade,
+    this.andar = () => {
+        // console.log(this.nome + ' tem ' + this.idade)
+    }
 }
 
-//window.onload = load
+function Dom(seletor){
+    
+    const elementsList = document.querySelectorAll(seletor)
+    this.elements = elementsList
+    
+    this.ativar = (classe) =>{
+        elementsList.forEach(element => {
+            element.classList.add(classe)
+        })
+    }
+    this.remover = ()=>{
+        elementsList.forEach(element => {
+            element.classList.remove(classe)
+        })
+    }
+}
+// const li = new Dom('li')
+// li.ativar('ativar')
+
+
+function Pessoa(nome, idade){
+
+    this.nome= nome,
+    this.idade= idade
+}
+
+
+Pessoa.prototype.andar = ()=>{
+    return 'Andou'
+}
+
+// const andre = new Pessoa('André', 28)
+
+// const lista = document.querySelectorAll('li')
+
+// const listaArray = Array.prototype.slice.call(lista)
+// const arrayList = Array.from(lista)
+
+function Outra(nome, sobrenome, idade){
+    this.nome = nome,
+    this.sobrenome = sobrenome,
+    this.idade = idade
+}
+
+Outra.prototype.nomeCompleto = ()=>{
+    return this.nome 
+}
+
+const name = new Outra('André', 'Pereira')
