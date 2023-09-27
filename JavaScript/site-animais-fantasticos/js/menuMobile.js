d545b0f16cf4c26b4c9161416fc4e82056bea4f5
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-09-26 22:03:26 -0300

    Update menuMobile.js

--- JavaScript/site-animais-fantasticos/js/menuMobile.js
+++ JavaScript/site-animais-fantasticos/js/menuMobile.js
@@ -2,40 +2,29 @@ import initMenuDropdown from './menuDropdown.js'
 
 export default function initMenuMobile(){
 
-}
-
-
-const buttonMenu = document.querySelector('.button-menu')
-const listMenu = document.querySelector('#menu')
-
-buttonMenu.addEventListener('click', toggleMenu)
-
-function toggleMenu(){
+    const buttonMenu = document.querySelector('.button-menu')
+    const listMenu = document.querySelector('#menu')
     
-    listMenu.classList.toggle('active')
-    buttonMenu.classList.toggle('active')
-
+    buttonMenu.addEventListener('click', toggleMenu)
+    
+    function toggleMenu(){
+        
+        listMenu.classList.toggle('active')
+        buttonMenu.classList.toggle('active')
+    
+    }
+    //verificar se clicou fora do botão e lista do menu
+    document.addEventListener('click', function(event){
+        let isListMenu = listMenu.contains(event.target)
+        let isButtonMenu = buttonMenu.contains(event.target)
+    
+        if(!isListMenu && !isButtonMenu){
+           listMenu.classList.remove('active')
+           buttonMenu.classList.remove('active')
+        }
+    })
 }
 
-document.addEventListener('click', function(event){
-    let isListMenu = listMenu.contains(event.target)
-    let isButtonMenu = buttonMenu.contains(event.target)
 
-    if(!isListMenu && !isButtonMenu){
-       listMenu.classList.remove('active')
-       buttonMenu.classList.remove('active')
-    }
-})
-
-// document.addEventListener('click', function(event) {
-//     // Verifica se o clique não foi no botão do menu ou dentro do menu
-//     if(!buttonMenu.contains(event.target) && !listMenu.contains(event.target)){
-//         // Fecha o menu se estiver aberto
-//         if(listMenu.classList.contains('active')){
-//             toggleMenu()
-//         }
-
-//     }
-// })
 
 
