c9033a3b9fb91ee8ced77d511f477b0bacb86fa9
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-09-26 22:02:28 -0300

    Update menuMobile.js

--- JavaScript/site-animais-fantasticos/js/menuMobile.js
+++ JavaScript/site-animais-fantasticos/js/menuMobile.js
@@ -4,17 +4,38 @@ export default function initMenuMobile(){
 
 }
 
+
 const buttonMenu = document.querySelector('.button-menu')
 const listMenu = document.querySelector('#menu')
 
-buttonMenu.addEventListener('click', openMenu)
+buttonMenu.addEventListener('click', toggleMenu)
 
-function openMenu(){
+function toggleMenu(){
     
     listMenu.classList.toggle('active')
     buttonMenu.classList.toggle('active')
-    
+
 }
 
+document.addEventListener('click', function(event){
+    let isListMenu = listMenu.contains(event.target)
+    let isButtonMenu = buttonMenu.contains(event.target)
+
+    if(!isListMenu && !isButtonMenu){
+       listMenu.classList.remove('active')
+       buttonMenu.classList.remove('active')
+    }
+})
+
+// document.addEventListener('click', function(event) {
+//     // Verifica se o clique não foi no botão do menu ou dentro do menu
+//     if(!buttonMenu.contains(event.target) && !listMenu.contains(event.target)){
+//         // Fecha o menu se estiver aberto
+//         if(listMenu.classList.contains('active')){
+//             toggleMenu()
+//         }
+
+//     }
+// })
 
 
