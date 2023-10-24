f5905ffed4bb83792d9044fc83eb503d12373e4a
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-10-23 21:24:28 -0300

    up

---
+++ JavaScript/Estudos/__LinhaDeComando/Comandos.js
@@ -0,0 +1,7 @@
+//cd Deskotp - Ir até a pasta
+//ls - mostrar o que tem na pasta
+//touch - criar arquivo
+//mkdir - criar pasta
+//rm - excluir arquivo
+//rm - r = excluir diretorio
+//cs .. = voltar a pasta
