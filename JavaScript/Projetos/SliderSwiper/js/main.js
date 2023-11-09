f8cfa388cedc61ae2b7d3ef730fc2c76e4822217
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-11-08 21:09:29 -0300

    up slider

---
+++ JavaScript/Projetos/SliderSwiper/js/main.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+    effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
