887ee4e9879663997b5d72c9dd4216c57b03365c
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-11-15 20:27:23 -0300

    up

--- React/Estudos/Hooks/readMe.js
+++
@@ -1,5 +0,0 @@
-//Hooks são uma nova adição ao React 16.8 que permitem que você use state e outras funcionalidades do React em componentes baseados em função, em vez de componentes de classe. Eles foram introduzidos para permitir o uso de estado e outras características do React em componentes funcionais, que antes eram limitados principalmente a lógica de apresentação.
-
-//useState
-//useEffect
-//useContext
--- React/Estudos/Vite/src/App.jsx
+++ React/Estudos/Vite/src/App.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
 
 const App = () => {
-  return <div>APP Reacts</div>;
-}
+  const dados = mario;
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((acc, item) => acc + item);
+
+  return (
+    <div>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'Ativa' : 'Inativo'}
+        </span>
+      </p>
+      <p>Total gasto: R$ {total}</p>
+      {total > 10000 && <p>Você está gastando muito</p>}
+    </div>
+  );
+};
 
 export default App;
