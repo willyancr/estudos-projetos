30c6cc1089cca928e79c4fe3b51f61dc9454c28c
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-11-16 19:01:44 -0300

    Update App.jsx

--- React/Estudos/Array/src/App.jsx
+++ React/Estudos/Array/src/App.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
-
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 const App = () => {
-  const livros = [
-    { nome: 'Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
+  const filterPreco = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
+  );
+
   return (
-    <ul>
-      {livros
-        .filter(({ano}) => ano >= 1998)
-        .map(({nome, ano}) => (
-          <li key={nome}>
-            {nome}, {ano}
-          </li>
-        ))}
-    </ul>
+    <section>
+      {filterPreco.map(({ id, nome, preco, cores }) => (
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li key={cor} style={{ background: cor, color: 'white' }}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
   );
 };
 
