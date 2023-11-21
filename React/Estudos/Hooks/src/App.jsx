7ccae921901e2a03818187631582874eb6dacb34
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-11-21 18:43:30 -0300

    up

--- React/Estudos/Hooks/src/App.jsx
+++ React/Estudos/Hooks/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import Produtos from './Desafio/Produtos';
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(true);
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText.toLowerCase()}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
   return (
-    <button onClick={() => setAtivo(!ativo)}>
-      {ativo ? 'Ativo' : 'Inativo'}
-    </button>
+    <>
+      <button onClick={handleClick} style={{ margin: '10px' }}>
+        Notebook
+      </button>
+      <button onClick={handleClick} style={{ margin: '10px' }}>
+        Smartphone
+      </button>
+      <button onClick={handleClick} style={{ margin: '10px' }}>
+        Tablet
+      </button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produtos dados={dados} />}
+    </>
   );
 };
 
