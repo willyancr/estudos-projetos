4cba130bb37766002b260269345d126f6419b228
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-11-27 17:05:25 -0300

    up

--- React/Estudos/Formularios/src/App.jsx
+++ React/Estudos/Formularios/src/App.jsx
@@ -1,32 +1,101 @@
-import React from 'react';
-import Produtos from './Desafio/Produtos';
+import React, { useEffect, useState } from 'react';
 
+const formFields = [
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    id: 'senha',
+    label: 'Senha',
+    type: 'password',
+  },
+  {
+    id: 'cep',
+    label: 'Cep',
+    type: 'text',
+  },
+  {
+    id: 'rua',
+    label: 'Rua',
+    type: 'text',
+  },
+  {
+    id: 'numero',
+    label: 'Numero',
+    type: 'text',
+  },
+  {
+    id: 'bairro',
+    label: 'Bairro',
+    type: 'text',
+  },
+  {
+    id: 'cidade',
+    label: 'Cidade',
+    type: 'text',
+  },
+  {
+    id: 'estado',
+    label: 'Estado',
+    type: 'text',
+  },
+];
 const App = () => {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [form, setForm] = useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
 
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText.toLowerCase()}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
+  //pegar valores dos inputs
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
   }
+  const [response, setResponse] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    }).then((response) => setResponse(response));
+  }
+
   return (
     <>
-      <button onClick={handleClick} style={{ margin: '10px' }}>
-        Notebook
-      </button>
-      <button onClick={handleClick} style={{ margin: '10px' }}>
-        Smartphone
-      </button>
-      <button onClick={handleClick} style={{ margin: '10px' }}>
-        Tablet
-      </button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produtos dados={dados} />}
+      <form onSubmit={handleSubmit}>
+        {formFields.map(({ id, label, type }) => (
+          <div key={id}>
+            <label htmlFor={id}>{label}</label>
+            <input
+              type={type}
+              id={id}
+              value={form[id]}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
+        {response && response.ok && <p>Formulario enviado com sucesso</p>}
+        {response && !response.ok && <p>Erro: Email ja cadastrado</p>}
+        <button>Enviar</button>
+      </form>
     </>
   );
 };
