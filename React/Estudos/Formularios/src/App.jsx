f6af0776296926eee93145be6635fbb2bff8963d
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-11-28 15:56:10 -0300

    up

--- React/Estudos/Formularios/src/App.jsx
+++ React/Estudos/Formularios/src/App.jsx
@@ -1,21 +1,58 @@
 import React, { useState } from 'react';
-import Inputs from './Inputs/Inputs';
-import Checkbox from './CheckBox/Checkbox';
-import Select from './Select/Select';
+
 import Input from './Inputs/Input';
 
 const App = () => {
   const [cep, setCep] = useState('');
+  const [msg, setMsg] = useState('');
+  const [send, setSend] = useState('');
+
+  function validacaoCEP(value) {
+    if (value.length === 0) {
+      setMsg('Preencha o CEP');
+      return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setMsg('Cep Invalido');
+      return false;
+    } else {
+      setMsg('');
+      return true;
+    }
+  }
+  function handleBlur({ target }) {
+    validacaoCEP(target.value);
+  }
+
+  function handleChange({ target }) {
+    if (msg) validacaoCEP(target.value);
+    setCep(target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if(validacaoCEP(cep)){
+      setSend('Cep enviado')
+    }else{
+      setSend('Cep não enviado')
+    }
+  }
+
   return (
     <>
-      <Input
-        label="CEP"
-        id="cep"
-        type="text"
-        value={cep}
-        setValue={setCep}
-        placeholder="0000-000"
-      />
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="CEP"
+          id="cep"
+          type="text"
+          value={cep}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          placeholder="0000-000"
+        />
+        <p>{msg}</p>
+        <button>Enviar</button>
+        <p>{send}</p>
+      </form>
     </>
   );
 };
--- React/Estudos/Formularios/src/Inputs/Input.jsx
+++ React/Estudos/Formularios/src/Inputs/Input.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Input = ({ label, id, type, value, setValue, ...props }) => {
+const Input = ({ label, id, type, value, onChange, ...props }) => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} name={id} value={value} {...props} />
+      <input
+        type={type}
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
     </>
   );
 };
