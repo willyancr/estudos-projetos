796640bb970443a6c19e283d12216550f80432c9
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-12-18 07:59:51 -0300

    Update App.jsx

--- React/Projetos/SiteDogs/src/App.jsx
+++ React/Projetos/SiteDogs/src/App.jsx
@@ -1,22 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import '../public/app.css';
-import Header from './Components/Header/Header';
-import Produto from './Components/Produtos/Produto';
-import Contato from './Components/Contato/Contato';
-import ProdutoDescricao from './Components/Produtos/ProdutoDescricao';
-import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Produto />} />
-        <Route path="contato" element={<Contato />} />
-        <Route path="produto/:id" element={<ProdutoDescricao />} />
+        <Route />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
