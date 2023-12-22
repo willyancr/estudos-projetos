5fba99b52ef52e08aaaee6e43122024f7d9e3dfa
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-12-21 22:27:50 -0300

    up

--- React/Projetos/SiteDogs/src/Components/Login/Login.jsx
+++ React/Projetos/SiteDogs/src/Components/Login/Login.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import styles from './Login.module.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <section className={styles.login}>
+    <div>
+      <img src="../image/dog_login.png" alt="" className={styles.responsiveImage}/>
+    </div>
+    <form className={styles.loginForm}>
+      <h1><span></span>Login</h1>
+      <label htmlFor="usuario">Usuário</label>
+      <input type="text" id='usuario' />
+      <label htmlFor="senha">Senha</label>
+      <input type="password" id='senha' />
+      <button>Entrar</button>
+    </form>
+
+  </section>;
 };
 
 export default Login;
