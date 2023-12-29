b6cf2404876bf16de2f3245d8cd9a3d0fd55dfe6
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2023-12-29 11:34:27 -0300

    Update Dashboard.jsx

--- React/Projetos/SiteDogs/src/Components/Account/Dashboard/Dashboard.jsx
+++ React/Projetos/SiteDogs/src/Components/Account/Dashboard/Dashboard.jsx
@@ -8,6 +8,12 @@ const Dashboard = () => {
         <h1 className="title">
           <span></span>Minha Conta
         </h1>
+        <nav>
+          <span><i className="fa-solid fa-house"></i></span>
+          <span><i className="fa-solid fa-chart-simple"></i></span>
+          <span><i className="fa-solid fa-plus fa-lg"></i></span>
+          <span><i className="fa-solid fa-right-from-bracket"></i></span>
+        </nav>
       </div>
     </section>
   );
