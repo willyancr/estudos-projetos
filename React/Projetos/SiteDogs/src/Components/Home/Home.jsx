1286ed4020c46acf8c9ac7d680e7598c9b746c02
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-01-05 15:39:03 -0300

    Update Home.jsx

--- React/Projetos/SiteDogs/src/Components/Home/Home.jsx
+++ React/Projetos/SiteDogs/src/Components/Home/Home.jsx
@@ -1,64 +1,31 @@
 import React from 'react';
 import styles from './Home.module.css';
+
 const Home = () => {
+  const images = [
+    { src: '../image/dog.jpg', views: Math.random() * 1000 },
+    { src: '../image/dog.jpg', views: Math.random() * 1000 },
+    { src: '../image/dog.jpg', views: Math.random() * 1000 },
+    { src: '../image/dog.jpg', views: Math.random() * 1000 },
+    { src: '../image/dog.jpg', views: Math.random() * 1000 },
+    { src: '../image/dog.jpg', views: Math.random() * 1000 },
+  ];
+
   return (
     <section className={`${styles.home} container efeito`}>
       <div>
         <ul className={styles.ulFeed}>
-          <div className={styles.imageDogsFeed}>
-            <li>
-              <img src="../image/dog.jpg" alt="" />
-            </li>
-            <span className={styles.views}>
-              <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
-              22540
-            </span>
-          </div>
-          <div className={styles.imageDogsFeed}>
-            <li>
-              <img src="../image/dog.jpg" alt="" />
-            </li>
-            <span className={styles.views}>
-              <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
-              22540
-            </span>
-          </div>
-          <div className={styles.imageDogsFeed}>
-            <li>
-              <img src="../image/dog.jpg" alt="" />
-            </li>
-            <span className={styles.views}>
-              <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
-              22540
-            </span>
-          </div>
-          <div className={styles.imageDogsFeed}>
-            <li>
-              <img src="../image/dog.jpg" alt="" />
-            </li>
-            <span className={styles.views}>
-              <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
-              22540
-            </span>
-          </div>
-          <div className={styles.imageDogsFeed}>
-            <li>
-              <img src="../image/dog.jpg" alt="" />
-            </li>
-            <span className={styles.views}>
-              <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
-              22540
-            </span>
-          </div>
-          <div className={styles.imageDogsFeed}>
-            <li>
-              <img src="../image/dog.jpg" alt="" />
-            </li>
-            <span className={styles.views}>
-              <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
-              22540
-            </span>
-          </div>
+          {images.map((image) => (
+            <div className={styles.imageDogsFeed}>
+              <li>
+                <img src={image.src} alt="" />
+              </li>
+              <span className={styles.views}>
+                <i className="fa-regular fa-eye" style={{ color: '#fff' }}></i>{' '}
+                {image.views.toFixed(1)}
+              </span>
+            </div>
+          ))}
         </ul>
       </div>
     </section>
