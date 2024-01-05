9207049023aeb56981940df1dc13f80ed6b46795
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-01-05 14:37:29 -0300

    up

--- React/Projetos/SiteDogs/src/Components/Account/Dashboard/Statistics.jsx
+++ React/Projetos/SiteDogs/src/Components/Account/Dashboard/Statistics.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './GlobalDashboard.css';
-import styles from './Statistics.module.css'
+import styles from './Statistics.module.css';
 import HeaderDashboard from './HeaderDashboard';
 
 const Statistics = () => {
@@ -10,6 +10,10 @@ const Statistics = () => {
       <section className="dashboardMain container efeito">
         <div className={styles.mainHeader}>
           <p>Acessos: 0</p>
+        </div>
+        <div className={styles.mainContainer}>
+          <div className={styles.left}>1</div>
+          <div className={styles.right}>1</div>
         </div>
       </section>
     </>
