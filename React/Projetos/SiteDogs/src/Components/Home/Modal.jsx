8e932fdaa7ad475d111ba8a0747bff13584e125b
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-01-08 22:53:24 -0300

    up

--- React/Projetos/SiteDogs/src/Components/Home/Modal.jsx
+++ React/Projetos/SiteDogs/src/Components/Home/Modal.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './Modal.module.css';
+import Input from '../Input/Input';
 
 const Modal = () => {
   return (
     <div className={styles.modal}>
       <div className={styles.modalContainer}>
-        <div className={styles.modalImg}> 
+        <div className={styles.modalImg}>
           <img src="../image/dog.jpg" alt="" />
         </div>
         <div className={styles.modalForm}>
           <p className={styles.modalViews}>
-            <a href="">@cat</a>
+            <a href="">@belinha</a>
             <span>
               <i className="fa-regular fa-eye"></i> 21568
             </span>
           </p>
-          <h1>Ellie</h1>
-          <ul>
+          <h1 className={styles.modalTitle}>
+            <span></span>Belinha
+          </h1>
+          <ul className={styles.modalDogInfos}>
             <li>| 5 kg</li>
             <li>| 09 anos</li>
           </ul>
@@ -27,8 +30,13 @@ const Modal = () => {
             <span>Linda essa dogzinha</span>
           </li>
         </ul>
-        <form>
-          <textarea name="comment" id="comment"></textarea>
+        <form className={styles.modalSendMsg}>
+          <textarea
+            name="comment"
+            id="comment"
+            placeholder="Comente..."
+          ></textarea>
+          <i className="fa-solid fa-square-arrow-up-right fa-2xl"></i>
         </form>
       </div>
     </div>
