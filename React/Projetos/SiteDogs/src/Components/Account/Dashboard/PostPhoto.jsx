0bbd1c7c05dbf5ac0f22f9d48734929a54924679
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-01-21 19:56:33 -0300

    Update PostPhoto.jsx

--- React/Projetos/SiteDogs/src/Components/Account/Dashboard/PostPhoto.jsx
+++ React/Projetos/SiteDogs/src/Components/Account/Dashboard/PostPhoto.jsx
@@ -5,18 +5,23 @@ import Input from '../../Input/Input';
 import styles from './PostPhoto.module.css';
 
 const PostPhoto = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
     <>
       <HeaderDashboard title="Poste sua foto" />
       <section className={`${styles.main} container dashboardMain efeito`}>
-        <form className={styles.mainForm}>
+        <form className={styles.mainForm} onSubmit={handleSubmit}>
           <label htmlFor="nome">Nome</label>
           <Input />
-          <label htmlFor="peso">Peso <span>(kg)</span></label>
-          <Input type='number'/>
+          <label htmlFor="peso">
+            Peso <span>(kg)</span>
+          </label>
+          <Input type="number" />
           <label htmlFor="idade">Idade</label>
           <Input />
-          <input type="file" name="" id="" />
+          <input type="file" name="img" id="img" />
           <Button name="Enviar" type="submit" />
         </form>
       </section>
