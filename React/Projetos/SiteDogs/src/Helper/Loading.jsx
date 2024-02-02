694010fe8228d8ca03041977f2f33d25fc2389c7
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-01 22:36:26 -0300

    criação da animação de loading

--- React/Projetos/SiteDogs/src/Helper/Loading.jsx
+++ React/Projetos/SiteDogs/src/Helper/Loading.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Loading.module.css'
 
 const Loading = () => {
   return (
-    <div>Carregando...</div>
+<div className={styles.loading}></div>
   )
 }
 
