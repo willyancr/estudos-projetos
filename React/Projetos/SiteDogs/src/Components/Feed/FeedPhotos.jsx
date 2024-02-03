6aea2393522304d4a3561248b752b130f56dc80f
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-03 01:24:55 -0300

    Update FeedPhotos.jsx

--- React/Projetos/SiteDogs/src/Components/Feed/FeedPhotos.jsx
+++ React/Projetos/SiteDogs/src/Components/Feed/FeedPhotos.jsx
@@ -8,25 +8,38 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 // Componente para exibir um feed de fotos
 const FeedPhoto = ({ setModalPhoto, user }) => {
-  const [page, setPage] = React.useState(1);
-  const [hasMore, setHasMore] = React.useState(true);
+  const [page, setPage] = React.useState(1); //controlar a página atual de fotos
+  const [hasMore, setHasMore] = React.useState(true); //determinar se há mais fotos para carregar
+  const [photos, setPhotos] = React.useState([]); //armazenar o array de fotos
+
   const { error, loading, request, data } = useFetch();
 
-  //Buscar fotos quando o componente é montado
+  //Buscar fotos quando o componente é montado ou quando a página é atualizada
   React.useEffect(() => {
     async function fetchPhotos() {
       const total = 6;
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
-      if (response && response.ok) {
+      if (response && response.ok) { // Se a resposta for OK, verifica se há mais fotos para carregar
         if (json.length < total) setHasMore(false);
       }
     }
     fetchPhotos();
   }, [request, user, page]);
 
-  const fetchMorePhotos = () => {
-    setPage((prevPages) => prevPages + 1);
+  const fetchMorePhotos = async () => {
+    const nextPage = page + 1;
+    const total = 6;
+    const { url, options } = PHOTOS_GET({ page: nextPage, total, user });
+    const { response, json } = await request(url, options);
+    if (response && response.ok) {
+      // Concatena as novas fotos com as fotos existentes
+      setPhotos((prevPhotos) => [...prevPhotos, ...json]);
+      setPage(nextPage);
+      if (json.length < total) {
+        setHasMore(false);
+      }
+    }
   };
 
   if (error) return <p>{error}</p>;
@@ -36,14 +49,14 @@ const FeedPhoto = ({ setModalPhoto, user }) => {
     <section>
       <div className={`${styles.home} container efeito`}>
         <InfiniteScroll
-          dataLength={data?.length || 0}
-          next={fetchMorePhotos}
-          hasMore={hasMore}
+          dataLength={data?.length || 0} // Comprimento atual da lista de fotos
+          next={fetchMorePhotos} // Função para carregar mais fotos
+          hasMore={hasMore} // Se ainda há mais fotos para carregar
           loader={<h4 style={{ textAlign: 'center' }}> Carregando mais fotos...</h4>}
         >
           <ul className={styles.ulFeed}>
-            {data &&
-              data.map((photo) => (
+            {photos &&
+              photos.map((photo) => (
                 <FeedPhotoItem
                   key={photo.id}
                   photo={photo}
