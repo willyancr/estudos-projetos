50840b2852e14c9b2411dac9b9c6673546eb195c
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-07 17:13:08 -0300

    up

--- React/Projetos/SiteDogs/src/Components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Button.module.css';
-
-const Button = ({ name, ...props }) => {
-  return <button className={styles.btn} {...props}>{name}</button>;
-};
-
-export default Button;
