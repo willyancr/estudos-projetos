83b27b8c80e8149c15175985c242b3b20fe805f4
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-11 22:09:25 -0300

    feat: aprendendo types e interface

--- Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Types-Interface/script.js
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Types-Interface/script.js
@@ -1,5 +1,15 @@
 "use strict";
-function dadosProdutos(produtos) {
-    console.log(produtos.nome, produtos.preco);
+async function fetchProdutos() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    console.log(data);
+    showProdutos(data);
 }
-console.log(dadosProdutos({ nome: 'Notebook', preco: 200 }));
+function showProdutos(data) {
+    document.body.innerHTML = `
+     <h2>Nome: ${data.nome}</h2>
+     <p>R$ ${data.preco}</p>
+     <p>Garantia: ${data.garantia} anos</p>
+     <p>Seguro de acidente: ${data.seguroAcidente ? 'Sim' : 'Não'}</p>
+   `;
+}
