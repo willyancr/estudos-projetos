719a638bf259167a741cea7d9219a8a80b66b0ee
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-13 08:11:53 -0300

    Update HTML and TypeScript files
    Add new links and button in the HTML file. Update TypeScript file to improve
    code readability and add explanatory comment.

--- Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Instaceof/script.ts
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Instaceof/script.ts
@@ -3,5 +3,5 @@ const link = document.getElementById('origamid');
 if (link instanceof HTMLAnchorElement) {
   link.href = link.href.replace('http://', 'https://');
 }
-
 //Esse código verifica se a variável link é uma instância de HTMLAnchorElement, que é o tipo de elemento usado para representar links em HTML. Se link for de fato um elemento HTMLAnchorElement, então o código atribui a URL 'https://www.origamid.com' ao atributo href desse elemento, redirecionando o link para o site 'https://www.origamid.com'.
+
