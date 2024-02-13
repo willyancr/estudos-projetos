a9dba1f939965aa5f0ea903971991c95eb224dfb
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-13 08:34:39 -0300

    Add initial HTML, CSS, and TypeScript files for InterfaceDOM
    Added index.html, script.js, script.ts, and tsconfig.json files for the
    InterfaceDOM project. The HTML file includes basic structure, CSS styling,
    and links to the script files. The TypeScript file contains a function to
    toggle link styles.

---
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/InterfaceDOM/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const links = document.querySelectorAll('.link');
+//criar uma função que deve ser executada para cada elemento e modificar através da função o estilo da cor e border
+function toggleLink(element) {
+    element.style.color = 'red';
+    element.style.border = '1px solid red';
+}
+links.forEach((link) => {
+    if (link instanceof HTMLElement) {
+        toggleLink(link);
+    }
+});
