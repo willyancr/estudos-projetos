a882bc455d8bba3b0b72994a929696bccf96e06e
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-13 10:43:15 -0300

    Add toggleMenu function for mobile menu button
    Add toggleMenu function to handle click event on mobile menu button, toggling
    the 'active' class on the navigation element and updating aria-label and
    aria-expanded attributes accordingly.

--- Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Eventos-Callback/script.js
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Eventos-Callback/script.js
@@ -1,1 +1,9 @@
 "use strict";
+const btnMobile = document.getElementById('btn-mobile');
+const nav = document.getElementById('nav');
+btnMobile?.addEventListener('click', toggleMenu);
+function toggleMenu() {
+    nav?.classList.toggle('active');
+    btnMobile?.setAttribute('aria-label', nav?.classList.contains('active') ? 'Fechar menu' : 'Abrir menu');
+    btnMobile?.setAttribute('aria-expanded', nav?.classList.contains('active') ? 'true' : 'false');
+}
