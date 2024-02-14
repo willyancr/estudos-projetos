c5f8e48c1d45a49258d84f380ad6fbd78eb5c0a3
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-14 16:49:44 -0300

    Update HTML and TypeScript files for Generic study
    Add link to Origamid website in HTML file and update TypeScript file with
    generic functions and async/await examples. Fix typo in function name.

--- Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Generic/script.js
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Generic/script.js
@@ -1,1 +1,37 @@
 "use strict";
+function retorno(param) {
+    return param;
+}
+const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const frutas = ['banana', 'maca', 'laranja', 'abacaxi', 'abacate'];
+function mostrar(param) {
+    return param.slice(0, 3);
+}
+console.log(mostrar(numero));
+console.log(mostrar(frutas));
+function tipoDado(param) {
+    const resultados = {
+        dado: param,
+        tipo: typeof param,
+    };
+    return resultados;
+}
+console.log(tipoDado(true));
+function extrairTexto(elemento) {
+    return {
+        texto: elemento.innerText,
+        elemento,
+    };
+}
+const a = document.querySelector('a');
+if (a)
+    console.log(extrairTexto(a).elemento.href);
+async function getFecth(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+async function handleData() {
+    const curso = await getFecth('https://api.origamid.dev/json/notebook.json');
+    console.log(curso);
+}
+handleData();
---
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/Generic/script.ts
@@ -0,0 +1,45 @@
+function retorno<T>(param: T): T {
+  return param;
+}
+
+const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const frutas = ['banana', 'maca', 'laranja', 'abacaxi', 'abacate'];
+function mostrar<T>(param: T[]) {
+  return param.slice(0, 3);
+}
+console.log(mostrar(numero));
+console.log(mostrar(frutas));
+
+function tipoDado<T>(param: T) {
+  const resultados = {
+    dado: param,
+    tipo: typeof param,
+  };
+  return resultados;
+}
+console.log(tipoDado(true));
+
+function extrairTexto<T extends HTMLElement>(elemento: T) {
+  return {
+    texto: elemento.innerText,
+    elemento,
+  };
+}
+const a = document.querySelector('a');
+if (a) console.log(extrairTexto(a).elemento.href);
+
+async function getFecth<T>(url: RequestInfo): Promise<T> {
+  const response = await fetch(url);
+  return await response.json();
+}
+interface Notebook {
+  nome: string;
+  preco: number;
+}
+async function handleData() {
+  const curso = await getFecth<Notebook>(
+    'https://api.origamid.dev/json/notebook.json',
+  );
+  console.log(curso);
+}
+handleData()
