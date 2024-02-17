ccdcf7f8786bdb589d8bd4744f9f7131947d07af
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-17 11:56:14 -0300

    Add example of using type guard with async API response and custom interface

--- Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/TypeGuard-ControlFlow/UserTypeGuard/script.js
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/TypeGuard-ControlFlow/UserTypeGuard/script.js
@@ -8,5 +8,33 @@ function handleData(data) {
     if (isString(data)) {
         console.log(data.toLocaleLowerCase());
     }
+    else {
+        console.log('Não é uma string');
+    }
 }
-handleData('EU SOU WILLYAN');
+//exemplo
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const data = await response.json();
+    console.log(data);
+    handleCurso(data);
+}
+fetchCursos();
+function isCurso(value) {
+    return (value instanceof Array &&
+        value.every((item) => item instanceof Object &&
+            'nome' in item &&
+            'horas' in item &&
+            'tags' in item));
+}
+function handleCurso(data) {
+    if (isCurso(data)) {
+        data.forEach((item) => {
+            document.body.innerHTML += `
+        <h2>Nome: ${item.nome}</h2>
+        <p>Horas: ${item.horas}</p>
+        <p>Tags: ${item.tags}</p>
+      `;
+        });
+    }
+}
--- Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/TypeGuard-ControlFlow/UserTypeGuard/script.ts
+++ Typescript-Iniciante/Estudos/1-Typescript-Iniciantes/TypeGuard-ControlFlow/UserTypeGuard/script.ts
@@ -7,7 +7,47 @@ function isString(value: unknown): value is string {
 function handleData(data: unknown) {
   if (isString(data)) {
     console.log(data.toLocaleLowerCase());
+  } else {
+    console.log('Não é uma string');
   }
 }
 
-handleData('EU SOU WILLYAN');
+//exemplo
+
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  console.log(data);
+  handleCurso(data);
+}
+fetchCursos();
+
+interface Curso {
+  nome: string;
+  horas: number;
+  tags: string[];
+}
+
+function isCurso(value: unknown): value is Curso[] {
+  return (
+    value instanceof Array &&
+    value.every(
+      (item) =>
+        item instanceof Object &&
+        'nome' in item &&
+        'horas' in item &&
+        'tags' in item,
+    )
+  );
+}
+function handleCurso(data: unknown) {
+  if (isCurso(data)) {
+    data.forEach((item) => {
+      document.body.innerHTML += `
+        <h2>Nome: ${item.nome}</h2>
+        <p>Horas: ${item.horas}</p>
+        <p>Tags: ${item.tags}</p>
+      `;
+    });
+  }
+}
