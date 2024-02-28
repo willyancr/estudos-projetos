28c19c297c9cf02c5643e00f88e8a41e93de3166
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-28 16:57:49 -0300

    Add table structure and fetch data from API
    Added table structure to display transaction data and implemented fetching data
    from an API to populate the table dynamically. Also adjusted tsconfig.json to
    allow unused locals and parameters.

--- Typescript-Iniciante/Praticas/Manipulacao-dados/src/main.ts
+++ Typescript-Iniciante/Praticas/Manipulacao-dados/src/main.ts
@@ -1,2 +1,35 @@
-import './style.css'
+import './style.css';
 
+async function fetchTransacoes() {
+  const response = await fetch('https://api.origamid.dev/json/transacoes.json');
+  const data = await response.json();
+  console.log(data);
+  mostrarTransacoes(data);
+}
+fetchTransacoes();
+
+interface Transacao {
+  nome: string;
+  valor: string;
+  status: string;
+  formaPagamento: string;
+  data: string;
+  email: string;
+}
+
+function mostrarTransacoes(transacoes: Transacao[]) {
+  const tabela = document.querySelector('#tabela tbody');
+  transacoes.forEach((transacao) => {
+    const transacaoHTML = `
+      <tr>
+        <td>${transacao.nome}</td>
+        <td>${transacao.status}</td>
+        <td>${transacao.data}</td>
+        <td>${transacao.formaPagamento}</td>
+        <td>${transacao.valor}</td>
+        <td>${transacao.email}</td>
+      </tr>
+    `;
+    if (tabela) tabela.innerHTML += transacaoHTML;
+  });
+}
