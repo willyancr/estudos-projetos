2393a1bb294d41fc04a0306684e03fa28a2b4f26
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-29 17:23:26 -0300

    Add totalCompras function to calculate total purchases
    Create a new function totalCompras to calculate the total purchases from a list
    of transactions. The function parses the transaction values, sums them up, and
    displays the total in Brazilian Real currency format on the webpage.

---
+++ Typescript-Iniciante/Praticas/Manipulacao-dados/src/total-compras.ts
@@ -0,0 +1,20 @@
+import { Transacao } from './interface-transacoes';
+
+export function totalCompras(transacoes: Transacao[]) {
+  let total = 0;
+  transacoes.forEach((transacao) => {
+    let valor = parseFloat(
+      transacao['Valor (R$)'].replace(/[^\d,]/g, '').replace(',', '.'),
+    );
+    if (!isNaN(valor)) {
+      total += valor;
+    }
+  });
+  const totalElement = document.querySelector(
+    '#total-compras',
+  ) as HTMLSpanElement;
+  totalElement.textContent = `${total.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  })}`;
+}
